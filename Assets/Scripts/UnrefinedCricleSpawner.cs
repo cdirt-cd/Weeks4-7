86781693b252679c7dee9b931a47c3c8923c7f84
caden768 <caden768@gmail.com>
2025-07-22 18:09:27 -0400

    Made a prefab for my assignment and an empty game object for spawning said prefab

---
+++ Assets/Scripts/UnrefinedCricleSpawner.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnrefinedCricleSpawner : MonoBehaviour
+{
+    private GameObject unrefinedCircle;
+
+
+
+    void Start()
+    {
+        GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
