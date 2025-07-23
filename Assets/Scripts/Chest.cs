9042b7db6342344fb55e68df95e87bcff558493a
cdirt-cd <caden768@gmail.com>
2025-07-23 16:45:02 -0400

    Realizing i forgot to make a lot of commits...Uhh tried coding gym stuff didn't do much for it but i managed to make a sprite move with arrow keys!

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chest : MonoBehaviour
+{
+    public TextMeshProUGUI pressE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //pressE = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+        //    pressE.gameObject.SetActive(false);
+
+        //}
+    }
+
+
+    //public void OnEPress()
+    //{
+    //    pressE.gameObject.SetActive(false);
+    //}
+}
