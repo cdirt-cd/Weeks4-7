c2b4c802520fc3fedf3cffd915d6d1d0688c39b5
cdirt-cd <caden768@gmail.com>
2025-07-27 20:39:27 -0400

    Added functionality to the UnrefinedCircleSpawner to let it destroy the UnrefinedCircles instead of having the Refiner handle that

--- Assets/Scripts/RefinerButton.cs
+++ Assets/Scripts/RefinerButton.cs
@@ -5,33 +5,63 @@ using UnityEngine.UI;
 
 public class RefinerButton : MonoBehaviour
 {
-    private bool buttonClicked = false;
-    public GameObject unrefinedCircle;
-    public List<GameObject> unrefinedCircles = new List<GameObject>();
+    //public bool buttonClicked = false;
+    //public float clickableRadius = 0.5f;
+
+    //public GameObject prefabUnrefinedCircle;
+    ////private GameObject spawnedUnrefinedCircle;
+
+    //public GameObject spawnedUnrefinedCircle;
+
+    //private Camera gameCamera;
+
+    //UnrefinedCircleScript unrefinedCircleScript;
+
+    //public List<GameObject> unrefinedCircles = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
 
-        //GameObject spawnedUnrefinedCircles = Instantiate(unrefinedCircle);
-        //unrefinedCircles.Add(spawnedUnrefinedCircles);
+        //gameCamera = Camera.main;
 
-        //GetComponent<UnrefinedCricleSpawner>().unrefinedCircles;
+        //unrefinedCircleScript = FindObjectOfType<UnrefinedCircleScript>();      
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(buttonClicked == true)
-        {
-            //Destroy(spawnedUnrefinedCircles);
-        }
+
+
+        //Vector3 inputPositionInScreenSpace = gameCamera.WorldToScreenPoint(transform.position);
+
+        //Vector3 inputPos = spawnedUnrefinedCircle.transform.position;
+        //Vector3 refinerPos = transform.position;
+        
+
+        //float distanceBetween = Vector3.Distance(inputPos, refinerPos);
+
+
+
+        //if (buttonClicked == true)
+        //{
+        //    if (distanceBetween <= clickableRadius)
+        //    {
+        //        Destroy(spawnedUnrefinedCircle);
+        //        buttonClicked = false;
+
+                
+        //    }
+
+
+        //}
     }
 
 
 
-    public void OnClick()
-    {
-        buttonClicked = true;
-    }
+    //public void OnClick()
+    //{
+    //    buttonClicked = true;
+        
+    //}
 }
--- Assets/Scripts/UnrefinedCricleSpawner.cs
+++ Assets/Scripts/UnrefinedCricleSpawner.cs
@@ -9,7 +9,16 @@ public class UnrefinedCricleSpawner : MonoBehaviour
 
     private GameObject spawnedUnrefinedCircle;
 
-    public List<GameObject> unrefinedCircles = new List<GameObject>();
+    //public List<GameObject> unrefinedCircles = new List<GameObject>();
+
+    
+
+    RefinerButton refinerButton;
+
+    private float clickableRadius = 1f;
+
+
+    private Camera gameCamera;
 
 
     void Start()
@@ -17,8 +26,9 @@ public class UnrefinedCricleSpawner : MonoBehaviour
         //Instantiate the unrefinedCircle at the position of this GameObject on start
         //Instantiate(unrefinedCircle, transform.position, Quaternion.identity);
 
-
+        gameCamera = Camera.main;
         spawnedUnrefinedCircle = Instantiate(prefabUnrefinedCircle, transform.position, Quaternion.identity);
+        refinerButton = FindObjectOfType<RefinerButton>();
 
         //spawnedObjects { spawnedObject[0] }
 
@@ -27,12 +37,74 @@ public class UnrefinedCricleSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 refinerPositionInScreenSpace = gameCamera.WorldToScreenPoint(refinerButton.transform.position);
+
+
+        
+
         if (spawnedUnrefinedCircle == null)
         {
             spawnedUnrefinedCircle = Instantiate(prefabUnrefinedCircle, transform.position, Quaternion.identity);
            
         }
 
+        //if (refinerButton != null)
+        //{
+        //    Vector3 spawnedUnrefinedCirclePos = spawnedUnrefinedCircle.transform.position;
+        //    Vector3 refinerPos = refinerButton.transform.position;
+
+
+        //    //float distanceBetween = Vector3.Distance(spawnedUnrefinedCirclePos, refinerPos);
+
+        //    if (Vector3.Distance(spawnedUnrefinedCirclePos, refinerPos) <= clickableRadius)
+        //    {
+
+        //        destroyDistance = true;
+
+               
+
+        //    }
+
+
+        //    if (buttonClicked == true && destroyDistance == true)
+        //    {
+        //        Destroy(spawnedUnrefinedCircle);
+        //        buttonClicked = false;
+        //        destroyDistance = false;
+        //        // Reset the distance check after destroying
+        //    }
+
+
+        //    //Debug.Log("Distance Between: " + distanceBetween);
+        //    //Debug.Log("Button Clicked: " + buttonClicked);
+        //    //Debug.Log("Spawned Unrefined Circle Position: " + spawnedUnrefinedCirclePos);
+
+        //    Debug.Log("destroyDistance: " + destroyDistance);
+        //}
 
     }
+
+    public void OnClick()
+    {
+        if (refinerButton != null)
+        {
+            Vector3 spawnedUnrefinedCirclePos = spawnedUnrefinedCircle.transform.position;
+            Vector3 refinerPos = refinerButton.transform.position;
+
+
+            //float distanceBetween = Vector3.Distance(spawnedUnrefinedCirclePos, refinerPos);
+
+            if (Vector3.Distance(spawnedUnrefinedCirclePos, refinerPos) <= clickableRadius)
+            {
+
+                Destroy(spawnedUnrefinedCircle);
+
+
+            }
+
+
+           
+        }
+    }
+
 }
