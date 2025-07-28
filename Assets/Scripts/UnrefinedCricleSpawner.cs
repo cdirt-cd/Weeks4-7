a91b0e83517add7c93293ae4b6c737723304b688
cdirt-cd <caden768@gmail.com>
2025-07-28 17:18:31 -0400

    Commenting my code

--- Assets/Scripts/UnrefinedCricleSpawner.cs
+++ Assets/Scripts/UnrefinedCricleSpawner.cs
@@ -7,6 +7,7 @@ public class UnrefinedCricleSpawner : MonoBehaviour
 {
     public Slider healthbarSlider;
 
+    //variables for health bar management
     public float maxHealth = 100;
     public float minHealth;
     private float currentHealth;
@@ -17,36 +18,45 @@ public class UnrefinedCricleSpawner : MonoBehaviour
 
     private GameObject spawnedUnrefinedCircle;
 
+    //scrapped list
     //public List<GameObject> unrefinedCircles = new List<GameObject>();
 
+    //GameObject class so the script knows what to instantiate
     public GameObject  prefabRefinedCircle;
 
     private GameObject spawnedRefinedCircle;
 
 
-
+    //RefinerButton class so the script can access its transform.position
     RefinerButton refinerButton;
 
+    //RefinedCircleSpawnLocation class so the refinedCircles spawn where I want them to
     RefinedCircleSpawnLocation refinedCircleSpawnLocation;
 
+    //float for the distance between the unrefinedCircle and the refinerButton that is acceptable for the unrefinedCircle to be refined
     private float clickableRadius = 1f;
 
-
+    //camera class for converting world coordinates to screen coordinates
     private Camera gameCamera;
 
 
     void Start()
     {
-        //Instantiate the unrefinedCircle at the position of this GameObject on start
-        //Instantiate(unrefinedCircle, transform.position, Quaternion.identity);
+     
 
+        //initialization of the camera to the main camera in the scene
         gameCamera = Camera.main;
+
+        //insatiation of an unrefinedCircle on start
         spawnedUnrefinedCircle = Instantiate(prefabUnrefinedCircle, transform.position, Quaternion.identity);
+
+        //initialization of the refinerButton and refinedCircleSpawnLocation so the script can access their transform.position
         refinerButton = FindObjectOfType<RefinerButton>();
+
         refinedCircleSpawnLocation = FindObjectOfType<RefinedCircleSpawnLocation>();
 
+        //code for the script to know what the inital health is
         currentHealth = maxHealth;
-
         healthbarSlider.value = currentHealth / maxHealth;
 
         //spawnedObjects { spawnedObject[0] }
@@ -56,13 +66,15 @@ public class UnrefinedCricleSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //screenspace stuff for transform things
         Vector3 refinerPositionInScreenSpace = gameCamera.WorldToScreenPoint(refinerButton.transform.position);
         Vector3 refinedCircleSpawnPositionInScreenSpace = gameCamera.WorldToScreenPoint(refinedCircleSpawnLocation.transform.position);
 
 
 
-        if (spawnedUnrefinedCircle == null)
+        if (spawnedUnrefinedCircle == null)//this checks to see if there is a unrefinedCircle, of there isn't it runs the code below
         {
+            //this instantiates a new unrefinedCircle
             spawnedUnrefinedCircle = Instantiate(prefabUnrefinedCircle, transform.position, Quaternion.identity);
            
         }
@@ -102,20 +114,23 @@ public class UnrefinedCricleSpawner : MonoBehaviour
         //    Debug.Log("destroyDistance: " + destroyDistance);
         //}
 
-
-        if (refinerButton != null)
+        //code for refining the unrefined circle when the refiner button is clicked and the unrefined circle is within the refiner
+        if (refinerButton != null) //this checks if the refiner button has been pressed and runs the code below
         {
+            //this code is for setting up Vector3.Distance stuff
             Vector3 spawnedUnrefinedCirclePos = spawnedUnrefinedCircle.transform.position;
             Vector3 refinerPos = refinerButton.transform.position;
 
-
+            //old variable that I scrapped due to being obsolete
             //float distanceBetween = Vector3.Distance(spawnedUnrefinedCirclePos, refinerPos);
 
-            if (Vector3.Distance(spawnedUnrefinedCirclePos, refinerPos) <= clickableRadius)
+
+            if (Vector3.Distance(spawnedUnrefinedCirclePos, refinerPos) <= clickableRadius)//This checks where the unrefinedCircle is in relation to the refiner
+                //then if it is within refiner it runs the code below
             {
-
+                //this destroys the UnrefinedCircle
                 Destroy(spawnedUnrefinedCircle);
-
+                //this spawns in a refinedCircle
                 spawnedRefinedCircle = Instantiate(prefabRefinedCircle, refinedCircleSpawnLocation.transform.position, Quaternion.identity);
 
             }
@@ -128,21 +143,23 @@ public class UnrefinedCricleSpawner : MonoBehaviour
 
     public void OnClick()
     {
-        //When a button is clicked this code runs
-        //To make sure that this code only runs
-        if (refinerButton != null)
+        //code for refining the unrefined circle when the refiner button is clicked and the unrefined circle is within the refiner
+        if (refinerButton != null) //this checks if the refiner button has been pressed and runs the code below
         {
+            //this code is for setting up Vector3.Distance stuff
             Vector3 spawnedUnrefinedCirclePos = spawnedUnrefinedCircle.transform.position;
             Vector3 refinerPos = refinerButton.transform.position;
 
-
+            //old variable that I scrapped due to being obsolete
             //float distanceBetween = Vector3.Distance(spawnedUnrefinedCirclePos, refinerPos);
 
-            if (Vector3.Distance(spawnedUnrefinedCirclePos, refinerPos) <= clickableRadius)
+
+            if (Vector3.Distance(spawnedUnrefinedCirclePos, refinerPos) <= clickableRadius)//This checks where the unrefinedCircle is in relation to the refiner
+                                                                                           //then if it is within refiner it runs the code below
             {
-
+                //this destroys the UnrefinedCircle
                 Destroy(spawnedUnrefinedCircle);
-
+                //this spawns in a refinedCircle
                 spawnedRefinedCircle = Instantiate(prefabRefinedCircle, refinedCircleSpawnLocation.transform.position, Quaternion.identity);
 
             }
@@ -151,12 +168,12 @@ public class UnrefinedCricleSpawner : MonoBehaviour
 
         }
     }
-    public void OnHealthChanged()
+    public void OnHealthChanged() //debugging code to see if the health bar is working
     {
         Debug.Log("Health has changed" + healthbarSlider.value.ToString());
     }
 
-    public void OnDamageClick()
+    public void OnDamageClick()  //debugging code to see if the health bar is working
     {
 
         currentHealth -= damage;
